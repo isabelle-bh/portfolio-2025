e8f459ee3ac305d6cae2351a8fd1a0db93ed58cb
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-11-24 22:06:18 -0500

    feat: Hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+export const HeroSection = () => {
+    return (
+        <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
+            <div className="container max-w-4xl mx-auto text-center z-10">
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+                        <span className="text-primary opacity-0 animate-fade-in-delay-2"> Isabelle</span>
+                        <span className="opacity-0 animate-fade-in-delay-3"> Beaudry</span>
+
+                    </h1>
+
+                    <p>
+                        I am coolio
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
