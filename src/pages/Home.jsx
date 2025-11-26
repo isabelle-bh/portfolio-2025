d6c62cd8c75bdd01ab0e6e2959fede68b8dec871
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-11-26 00:20:02 -0500

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "@/components/SkillsSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import { ContactSection } from "@/components/ContactSection";
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/Footer";
 
 export const Home = () => {
 
