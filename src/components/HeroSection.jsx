38ac07644d80992490c45a367a619d9d0a344d24
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-11-25 20:04:38 -0500

    fix: anchor link

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
                     </p>
 
                     <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-                        <a href="projects" className="cosmic-button">View My Work</a>
+                        <a href="#projects" className="cosmic-button">View My Work</a>
                     </div>
                 </div>
             </div>
