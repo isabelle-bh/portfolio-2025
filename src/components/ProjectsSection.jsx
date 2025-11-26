3b7071099692417b2890985e5f669f3f4e52c9de
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-11-25 23:02:49 -0500

    style: rearranging projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -6,7 +6,7 @@ const projects = [
         title: "Circuit Sync",
         description: "An iOS app built with SwiftUI that syncs live Formula 1 race data from the OpenF1 API, giving fans an easy way to track circuits, race dates, and details in one place.",
         image: "/projects/circuit-sync-1.png",
-        tags: ["Swift", "iOS", "Xcode"],
+        tags: ["Swift", "iOS", "Xcode", "Mobile Dev"],
         demoUrl: "#",
         githubUrl: "https://github.com/isabelle-bh/CircuitSync",
     },
@@ -15,45 +15,45 @@ const projects = [
         title: "Sky Courier",
         description: "A casual Unity game that combines playful 3D visuals and motivational quotes. Players click on a rotating planet to send out a plane that flies towards the player, delivers an uplifting message, and then returns to the planet once dismissed. Features a world-space main menu, smooth camera transitions, lofi-music player, and randomized quotes.",
         image: "/projects/sky-courier-1.png",
-        tags: ["Unity3D", "C#"],
+        tags: ["Unity3D", "C#", "Game Dev"],
         demoUrl: "#",
         githubUrl: "https://github.com/isabelle-bh/Sky-Courier",
     },
     {
         id: 3,
-        title: "STELLA",
-        description: "Simulated Training Environments & Large Learning Automata (STELLA) is an autonomous driving project which demonstrates the application of simulation environments as a testing ground for imitation learning autonomous agents. Part of the work reproduces and validates the current state of the art model in simulation agents, Transfuser++, then proposes modifications in an attempt to improve its results. We run all agents in the CARLA simulator, which has an expansive community of tests, benchmarks, and agents. Evaluation is performed with the CARLA Leaderboard 2.0 framework, which sets updated standards on route completion, infractions, and minimum speed. Additionally, the project contributes to the open source community by adapting and improving existing tools in autonomous driving.",
-        image: "/projects/stella-1.png",
-        tags: ["CARLA", "Python", "PyTorch", "HuggingFace", "CUDA"],
-        demoUrl: "#",
-        githubUrl: "https://github.com/isabelle-bh/STELLA",
-    },
-    {
-        id: 4,
         title: "Women in Science UWO Website",
         description: "An interactive React website for the Women in Science club at Western University. Team project made using React, HTML, CSS, and JavaScript.",
         image: "/projects/wis-uwo-1.png",
-        tags: ["React", "JavaScript", "HTML/CSS", "Bootstrap","GitHub Pages"],
+        tags: ["React", "JavaScript", "HTML/CSS", "Bootstrap","GitHub Pages", "Web Dev"],
         demoUrl: "https://www.wisuwo.com/",
         githubUrl: "https://github.com/wisuwo/wis",
     },
-        {
+    {
+        id: 4,
+        title: "Strawberry Shores",
+        description: "This is a 2D adventure RPG which takes place in a village called Strawberry Shores. It combines both puzzle and combat elements for an enjoyable experience. Team project made using Unity / C#. Credits to all assets / tutorials used can be found in the README.",
+        image: "/projects/strawberry-shores-1.png",
+        tags: ["Unity2D", "C#", "Game Dev"],
+        demoUrl: "#",
+        githubUrl: "https://github.com/isabelle-bh/StrawberryShores",
+    },
+    {
         id: 5,
-        title: "my app",
+        title: "Superhero Database",
         description: "A full stack superhero database application with login/signup, admin, user management, list creation functionalities, and more. Solo project made using JavaScript, HTML + CSS, MongoDB, Express, React, and Node.",
         image: "/projects/superhero-db-2.png",
-        tags: ["React", "JavaScript", "Node.js", "Express", "MongoDB", "AWS"],
+        tags: ["React", "JavaScript", "Node.js", "Express", "MongoDB", "AWS", "Fullstack"],
         demoUrl: "#",
         githubUrl: "https://github.com/isabelle-bh/Superhero-Database",
     },
-        {
+    {
         id: 6,
-        title: "Strawberry Shores",
-        description: "This is a 2D adventure RPG which takes place in a village called Strawberry Shores. It combines both puzzle and combat elements for an enjoyable experience. The player must complete a series of quests given to them by the village Wizard in order to save the village from a terrible fate. Team project made using Unity / C#. Credits to all assets / tutorials used can be found in the README.",
-        image: "/projects/strawberry-shores-1.png",
-        tags: ["Unity2D", "C#"],
+        title: "STELLA",
+        description: "Simulated Training Environments & Large Learning Automata (STELLA) is an autonomous driving project which demonstrates the application of simulation environments as a testing ground for imitation learning autonomous agents.",
+        image: "/projects/stella-1.png",
+        tags: ["CARLA", "Python", "PyTorch", "HuggingFace", "CUDA", "AI"],
         demoUrl: "#",
-        githubUrl: "https://github.com/isabelle-bh/StrawberryShores",
+        githubUrl: "https://github.com/isabelle-bh/STELLA",
     },
 ]
 
@@ -65,7 +65,7 @@ export const ProjectsSection = () => {
                     Featured <span className="text-primary">Projects</span>
                 </h2>
                 <p className="text-muted-foreground text-center mb-12 max-w-2xl mx-auto">
-                    Here are some of my recent projects
+                    Here are some of my recent projects!
                 </p>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {projects.map((project, key) => (
@@ -86,9 +86,11 @@ export const ProjectsSection = () => {
                                 <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
                                 <div className="flex justify-between items-center">
                                     <div className="flex space-x-3">
-                                        <a href={project.demoUrl} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                                            <ExternalLink size={20}/>
-                                        </a>
+                                        {project.demoUrl !== "#" && (
+                                            <a href={project.demoUrl} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                                <ExternalLink size={20}/>
+                                            </a>
+                                        )}
                                         <a href={project.githubUrl} target="_blank" className="text-foreground/80 hover:text-primary transition-colors duration-300">
                                             <Github size={20}/>
                                         </a>
